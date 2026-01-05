be3a5ddbe0d2d29bb6c7b0fca5dd69de88ef955e
AuliaDiva1 <161435778+AuliaDiva1@users.noreply.github.com>
2026-01-05 20:42:50 +0700

    Update page.js

--- fe-nextjs-siakad/app/(main)/master/siswa/page.js
+++ fe-nextjs-siakad/app/(main)/master/siswa/page.js
@@ -9,7 +9,7 @@ import dynamic from "next/dynamic";
 import { ConfirmDialog, confirmDialog } from "primereact/confirmdialog";
 import ToastNotifier from "../../../components/ToastNotifier";
 import HeaderBar from "../../../components/headerbar";
-import FilterTanggal from "../../../components/filterTanggal";
+import FilterTanggal from "@/components/filterTanggal";
 import TabelSiswa from "./components/tabelSiswa";
 import FormDialogSiswa from "./components/FormDialogSiswa";
 import AdjustPrintMarginLaporan from "./print/AdjustPrintMarginLaporan";
